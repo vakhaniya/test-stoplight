33e12617c46001ad4a736ec9b0dc588417b6052d
vakhaniya-pod <gennady@podscribe.com>
2026-01-16 17:24:32 +0300

    Delete .spectral.mjs

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwMDUzOQ/spectral.js?branch_slug=main&token=9e55f8d4-7a6f-445d-8535-9ba6685ab340";
-export default { extends: ruleset };
